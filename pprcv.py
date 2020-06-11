2c6b549e59bb5f925b36bd373d927f11c4deb52e
charles.chang <charles.chang@royaltek.com>
2020-06-11 17:34:44 +0800

    bug: cannot receive more files

--- pprcv.py
+++ pprcv.py
@@ -12,15 +12,16 @@ while True:
 	fname = sc.recv(1024)
 	f = open(fname,'wb')
 	print(fname)
+	sc.close()
 
 	sc, address = s.accept()
 	print(address)
 	
-	while True:
+	l = sc.recv(1024)
+	while (l):
+		f.write(l)
 		l = sc.recv(1024)
-		while (l):
-			f.write(l)
-			l = sc.recv(1024)
 	f.close()
 	sc.close()
+	print("OK")
 s.close()
